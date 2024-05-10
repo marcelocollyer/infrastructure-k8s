0a6f0532c4639f334309b2183fa6d80764e04181
Marcelo Collyer <marcelocollyer@gmail.com>
2024-05-09 23:59:01 -0400

    adding manifests

---
+++ init.sh
@@ -0,0 +1,27 @@
+# Run this script once to install argocd and istio
+# all the further updates will deploy automatically via argocd upon new tags on main branch
+k3d cluster delete
+k3d cluster create -p "8001:30000@loadbalancer" --agents 2
+
+# Install Istio and include it to PATH
+eval "$(curl -L https://istio.io/downloadIstio | sh - | sed -n '/export PATH.*/p')"
+
+# Creates namespace where the apps will run
+kubectl create namespace apps
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl apply -f argocd-app-deploy.yaml
+
+# Install Istio and set it to work with default namespace (use specific namespaces for your apps in prod!)
+kubectl create namespace istio-system
+istioctl install -y
+kubectl label namespace apps istio-injection=enabled
+
+# addons
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/addons/kiali.yaml
+
+echo "Argo Password: $(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)"
